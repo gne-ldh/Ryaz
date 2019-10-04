2fd3aa0fa4eaef4e35aa28e01035eb76dc516373
Divyanshu Garg <divyanshugarg36@gmail.com>
2019-10-05 00:36:50 +0530

    changes

--- set.php
+++ set.php
@@ -1,62 +1,86 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
 
 
-      <style type="text/css">
-        
-        li{
-          list-style-type: none;
-          text-decoration: none;
-          display:inline;
-          font-size: 130%;
-        }
-      </style>
+  <style type="text/css">
+    li {
+      list-style-type: none;
+      text-decoration: none;
+      display: inline;
+      font-size: 130%;
+    }
+
+    .header {
+      font-family: arial
+    }
+  </style>
 
 
 
 
 
-  </head>
+</head>
 
-<body style="margin:0 10% 0 10%;font-family:arial ">
+<body style="margin:0 10% 0 10%;">
 
-  <div style="text-align:center">
-    <p style="font-size:180%;font-weight:bold;">NAME OF EXAM</p>  
-  <p style="font-size:130%">Subject Code :</p>
-   <p style="font-size:130%">M Code :</p>
-  </div> 
+
+  <div class="header">
+
+    <div style="text-align:center">
+      <p style="font-size:180%;font-weight:bold;">Exam Name</p>
+      <p style="font-size:130%">Subject Code :</p>
+      <p style="font-size:130%">M Code :</p>
+    </div>
+
+    <div>
+      <li>Time Duration: 3 Hours</li>
+      <li style="float:right;">Max. Marks: 60</li>
+      <br><br>
+      <p style="font-size:120%;text-align:left;font-weight:bold;">INSTRUCTION TO CANDIDATES :
+        <p style="font-size:120%;text-align:left;font-weight:bold;">1. SECTION-A is COMPULSORY consisting of TEN questions carrying TWO marks
+          each.</p>
+        <p style="font-size:120%;text-align:left;font-weight:bold;">2. SECTION-B contains FIVE questions carrying FIVE marks each and students
+          have to attempt any FOUR questions.</p>
+        <p style="font-size:120%;text-align:left;font-weight:bold;">3. SECTION-C contains THREE questions carrying TEN marks each and students
+          have to attempt any TWO questions.</p>
+      </p>
+    </div>
+
+  </div>
+
+
+
+  <br>
 
   <div>
-    <li>Timing :</li>
-    <li style="float:right;">Max. Marks :</li>
-<br><br>
-    <p style="font-size:120%;text-align:left;font-weight:bold;">INSTRUCTION TO CANDIDATES :
-      <li>1. SECTION-A is COMPULSORY consisting of TEN questions carrying TWO marks
-      each.</li>
-      <li>2. SECTION-B contains FIVE questions carrying FIVE marks each and students
-      have to attempt any FOUR questions.</li>
-      <li>3. SECTION-C contains THREE questions carrying TEN marks each and students
-      have to attempt any TWO questions.</li>
-    </p>
-  </div> 
-<br>
- <div>
-  <p style="font-size:120%;font-weight:bold;">2 marks questions</p>
-  <p style="font-size:110%;">i.</p>
-  <p style="font-size:110%;">ii.</p>
-  <p style="font-size:110%;">iii.</p>
-  <p style="font-size:110%;">iv.</p>
-  <p style="font-size:110%;">v.</p>
-  <p style="font-size:110%;">vi.</p>
-  <p style="font-size:110%;">vii.</p>
-  <p style="font-size:110%;">viii.</p>
-  <p style="font-size:110%;">ix.</p>
-  <p style="font-size:110%;">x.</p>
-  </div>  
+    <p style="font-size:150%;text-align: center;font-weight: bold">Section A</p>
+  </div>
+
+  <div>
+    <p style="font-size:110%;">Q1</p>
+  </div>
+
+  <div>
+    <p style="font-size:150%;text-align: center;font-weight: bold">Section B</p>
+  </div>
+
+  <div>
+    <p style="font-size:110%;">Q11</p>
+  </div>
+
+  <div>
+    <p style="font-size:150%;text-align: center;font-weight: bold">Section C</p>
+  </div>
+
+  <div>
+    <p style="font-size:110%;">Q16</p>
+  </div>
 
 
 
 
 </body>
+
 </html>
---
+++ view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Question paper</title>
+
+    <style>
+    body{
+        margin: 0;
+    }
+    .container{
+        margin: 7%;
+        border: solid 1px;
+        padding: 10px;
+        border-radius: 3px;
+    }
+    </style>
+
+</head>
+<body>
+    <div class="container">
+    <?php
+    include 'set.php';
+    ?>
+    </div>
+</body>
+</html>

e8a1615e0fa5947d4a42366efcf2f0740e73c682
Divyanshu Garg <divyanshugarg36@gmail.com>
2019-10-04 21:39:09 +0530

    changes

---
+++ set.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+
+      <style type="text/css">
+        
+        li{
+          list-style-type: none;
+          text-decoration: none;
+          display:inline;
+          font-size: 130%;
+        }
+      </style>
+
+
+
+
+
+  </head>
+
+<body style="margin:0 10% 0 10%;font-family:arial ">
+
+  <div style="text-align:center">
+    <p style="font-size:180%;font-weight:bold;">NAME OF EXAM</p>  
+  <p style="font-size:130%">Subject Code :</p>
+   <p style="font-size:130%">M Code :</p>
+  </div> 
+
+  <div>
+    <li>Timing :</li>
+    <li style="float:right;">Max. Marks :</li>
+<br><br>
+    <p style="font-size:120%;text-align:left;font-weight:bold;">INSTRUCTION TO CANDIDATES :
+      <li>1. SECTION-A is COMPULSORY consisting of TEN questions carrying TWO marks
+      each.</li>
+      <li>2. SECTION-B contains FIVE questions carrying FIVE marks each and students
+      have to attempt any FOUR questions.</li>
+      <li>3. SECTION-C contains THREE questions carrying TEN marks each and students
+      have to attempt any TWO questions.</li>
+    </p>
+  </div> 
+<br>
+ <div>
+  <p style="font-size:120%;font-weight:bold;">2 marks questions</p>
+  <p style="font-size:110%;">i.</p>
+  <p style="font-size:110%;">ii.</p>
+  <p style="font-size:110%;">iii.</p>
+  <p style="font-size:110%;">iv.</p>
+  <p style="font-size:110%;">v.</p>
+  <p style="font-size:110%;">vi.</p>
+  <p style="font-size:110%;">vii.</p>
+  <p style="font-size:110%;">viii.</p>
+  <p style="font-size:110%;">ix.</p>
+  <p style="font-size:110%;">x.</p>
+  </div>  
+
+
+
+
+</body>
+</html>
